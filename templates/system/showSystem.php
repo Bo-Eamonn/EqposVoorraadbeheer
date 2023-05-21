39c7013028169e0e2874c699cb685b962f8aafef
Bo-Eamonn <bo.eamonn@gmail.com>
2023-05-21 23:21:53 +0200

    Bo | Added a fallback for when there is no data to be displayed

--- templates/system/showSystem.php
+++ templates/system/showSystem.php
@@ -58,48 +58,53 @@ require_once "templates/header.php";
                 </thead>
                 <tbody>
                     <?php
-                        foreach ($result as $system) {
-                            echo '<tr>';
-                            echo '<td>'. $system->model .'</td>';
-                            echo '<td>'. $system->sn. '</td>';
-                            echo '<td>'. $system->status .'</td>';
-                            echo '<td>'. $system->firm .'</td>';
-                            echo '<td>'. $system->issue .'</td>';
-                            echo '<td>'. $system->ticketed .'</td>';
-                            echo '<td>'. $system->notes .'</td>';
-                            echo '<td>';
-                            echo '<form action="" method="post">';
-                            echo '<button class="btn btn-outline-success" type="submit" value="'. $system->id .'" name="showUpdateSystem" title="Edit">';
-                            echo '<i class="bi bi-pencil"></i>';
-                            echo '</button>';
-                            echo '<!-- Button trigger modal -->';
-                            echo '<button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#systemModal'. $system->id .'">';
-                            echo '<i class="bi bi-trash"></i>';
-                            echo '</button>';
-                            echo '</form>';
-                            echo '</td>';
-                            echo '</tr>';
-                            echo '<!-- Modal'. $system->id .' -->
-                            <div class="modal fade" id="systemModal'. $system->id .'" tabindex="-1" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5">Weet je het zeker?</h1>
+                if (empty($result)) {
+                    echo '<p>No data to display.</p>';
+                } else {
+                    foreach ($result as $system) {
+                        echo '<tr>';
+                        echo '<td>'. $system->model .'</td>';
+                        echo '<td>'. $system->sn. '</td>';
+                        echo '<td>'. $system->status .'</td>';
+                        echo '<td>'. $system->firm .'</td>';
+                        echo '<td>'. $system->issue .'</td>';
+                        echo '<td>'. $system->ticketed .'</td>';
+                        echo '<td>'. $system->notes .'</td>';
+                        echo '<td>';
+                        echo '<form action="" method="post">';
+                        echo '<button class="btn btn-outline-success" type="submit" value="'. $system->id .'" name="showUpdateSystem" title="Edit">';
+                        echo '<i class="bi bi-pencil"></i>';
+                        echo '</button>';
+                        echo '<!-- Button trigger modal -->';
+                        echo '<button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#systemModal'. $system->id .'">';
+                        echo '<i class="bi bi-trash"></i>';
+                        echo '</button>';
+                        echo '</form>';
+                        echo '</td>';
+                        echo '</tr>';
+                        echo '<!-- Modal'. $system->id .' -->
+                        <div class="modal fade" id="systemModal'. $system->id .'" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5">Weet je het zeker?</h1>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Weet je het zeker om systeem <b>'. $system->model .'</b> met serie nummer <b>'. $system->sn .'</b> te verwijderen?</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <form action="" method="post" class="container-fluid row">
+                                            <button type="button" class="col-sm align-self-start btn btn-success " data-bs-dismiss="modal">Annuleer</button>
+                                            <button type="submit" class="col-sm align-self-end btn btn-danger" type="submit" value="'. $system->id .'" name="deleteSystem" title="Remove"><i class="bi bi-trash"></i> Verwijder</button>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="modal-body">
-                                <p>Weet je het zeker om systeem <b>'. $system->model .'</b> met serie nummer <b>'. $system->sn .'</b> te verwijderen?</p>
-                            </div>
-                            <div class="modal-footer">
-                                <form action="" method="post" class="container-fluid row">
-                                <button type="button" class="col-sm align-self-start btn btn-success " data-bs-dismiss="modal">Annuleer</button>
-                                <button type="submit" class="col-sm align-self-end btn btn-danger" type="submit" value="'. $system->id .'" name="deleteSystem" title="Remove"><i class="bi bi-trash"></i> Verwijder</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>';
-                        }
-?>
+                        </div>';
+                    }
+                }
+                ?>
+
 
             </table>
         </div>
